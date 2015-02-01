a1e3caec490796a8bbf0a1937a16a722ee5169e0
mero <piotr.luczko@gmail.com>
2015-02-02 00:12:07 +0100

    fix notification import

--- SimpleChatClient/src/com/simplechatclient/android/TabsActivity.java
+++ SimpleChatClient/src/com/simplechatclient/android/TabsActivity.java
@@ -31,7 +31,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
